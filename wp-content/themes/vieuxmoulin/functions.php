db321e7d261865affaf6e028c570137abc2ad6f3
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2025-05-07 13:49:20 +0200

    add function

---
+++ wp-content/themes/vieuxmoulin/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once('acf.php');
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+include_once __DIR__ . '/disable-gutenberg.php';
+include_once __DIR__ . '/remove-default-actions.php';
+include_once __DIR__ . '/vite-manifest.php';
+include_once __DIR__ . '/srcset-create.php';
---
+++ wp-content/themes/vieuxmoulin/remove-default-actions.php
@@ -0,0 +1,14 @@
+<?php
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('wp_head', 'feed_links_extra', 3);
+remove_action('wp_head', 'wp_oembed_add_discovery_links');
+remove_action('wp_head', 'wp_oembed_add_host_js');
+remove_action('wp_head', 'rest_output_link_wp_head');
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'rsd_link');
+add_filter('show_admin_bar', '__return_false');
+
+
+
+
