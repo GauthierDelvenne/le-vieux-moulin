3f754de6626d4b8dfe2e5c62affcafc164b837ae
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2025-05-11 22:50:15 +0200

    Update flexible.php

--- wp-content/themes/vieuxmoulin/templates/content/flexible.php
+++ wp-content/themes/vieuxmoulin/templates/content/flexible.php
@@ -4,8 +4,16 @@
       include('text-media/text-media.php');
     elseif (get_row_layout() === 'text'):
       include('text/text.php');
+    elseif (get_row_layout() === 'media'):
+      include('text-media/media.php');
+    elseif (get_row_layout() === 'double-text-media'):
+        include('text-media/double-text-media.php');
+    elseif (get_row_layout() === 'landing-page'):
+      include('landing-page/landing-page.php');
     elseif (get_row_layout() === 'list'):
       include('list/list.php');
+    elseif (get_row_layout() === 'background-list'):
+      include('list/background-list.php');
     elseif (get_row_layout() === 'gallery'):
       include('gallery/gallery.php');
     elseif (get_row_layout() === 'image'):
