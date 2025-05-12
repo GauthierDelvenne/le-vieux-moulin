8195831f9ef04cec6a0d1ee0d45954ed8e8c438f
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2025-05-12 15:30:37 +0200

    add button template

---
+++ wp-content/themes/vieuxmoulin/templates/content/button/button.php
@@ -0,0 +1,40 @@
+<?php $image = get_sub_field('image'); ?>
+<?php $button1 = get_sub_field('text_button_1'); ?>
+<?php $button2 = get_sub_field('text_button_2'); ?>
+<?php $class = get_sub_field('class'); ?>
+<?php
+$current_path = $_SERVER['REQUEST_URI'];
+
+function getPathFromUrl($url) {
+    $parsed = parse_url($url);
+    return $parsed['path'] ?? '/';
+}
+?>
+
+<section class="button__section_container section_container_<?= $class !== '' ? $class : '' ?>">
+    <?php if (!empty($image)): ?>
+        <div class="button-media">
+            <?= responsive_image($image, ['classes' => 'button__image', 'lazy' => 'lazy']) ?>
+        </div>
+    <?php endif; ?>
+    <div class="button-container">
+        <?php if (!empty($button1)): ?>
+            <p class="button-button1">
+                <a class="button__content-link <?= ($current_path == getPathFromUrl($button1['url'])) ? 'active' : 'inactive' ?>"
+                   href="<?= $button1['url'] ?>"
+                   title="<?= $button1['title'] ?>">
+                    <?= $button1['title'] ?>
+                </a>
+            </p>
+        <?php endif; ?>
+        <?php if (!empty($button2)): ?>
+            <p class="button-button2">
+                <a class="button__content-link <?= ($current_path == getPathFromUrl($button2['url'])) ? 'active' : 'inactive' ?>"
+                   href="<?= $button2['url'] ?>"
+                   title="<?= $button2['title'] ?>">
+                    <?= $button2['title'] ?>
+                </a>
+            </p>
+        <?php endif; ?>
+    </div>
+</section>
