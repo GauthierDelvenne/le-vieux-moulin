8f7a93ef7a201a82bacbff907769b716a99080a5
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2025-05-18 17:22:26 +0200

    fix multiple used function

--- wp-content/themes/vieuxmoulin/templates/content/text-media/double-text-media.php
+++ wp-content/themes/vieuxmoulin/templates/content/text-media/double-text-media.php
@@ -13,69 +13,72 @@
         <?php endif; ?>
 
 
-        <?php
-        function render_text_media_block($block): void
-        {
-            if (!$block) return;
+        <?php if (!function_exists('render_text_media_block')) {
 
-            $headline = $block['headline'] ?? '';
-            $text = $block['text'] ?? '';
-            $cta = $block['cta'] ?? '';
-            $image = $block['image'] ?? '';
-            $media_position = $block['media_position'] ?? '';
-            $class = $block['class'] ?? '';
-            $shape_left = $block['shape_left'] ?? '';
-            $shape_right = $block['shape_right'] ?? '';
+            function render_text_media_block($block): void
+            {
+                if (!$block) return;
 
-            ?>
-            <div class="double-text-media-content">
-                <section class="text-media__section_container section_container_<?= esc_attr($class) ?>">
-                    <?php if (!empty($headline)) : ?>
-                        <h3 class="text-media__content-headline" aria-level="3">
-                            <?= esc_html($headline) ?>
-                        </h3>
-                    <?php endif; ?>
+                $headline = $block['headline'] ?? '';
+                $text = $block['text'] ?? '';
+                $cta = $block['cta'] ?? '';
+                $image = $block['image'] ?? '';
+                $media_position = $block['media_position'] ?? '';
+                $class = $block['class'] ?? '';
+                $shape_left = $block['shape_left'] ?? '';
+                $shape_right = $block['shape_right'] ?? '';
 
-                    <div class="text-media__content-container">
-                        <?php if (!empty($text)) : ?>
-                            <div class="text-media__content-text">
-                                <?= wp_kses_post($text) ?>
+                ?>
+                <div class="double-text-media-content">
+                    <section class="text-media__section_container section_container_<?= esc_attr($class) ?>">
+                        <?php if (!empty($headline)) : ?>
+                            <h3 class="text-media__content-headline" aria-level="3">
+                                <?= esc_html($headline) ?>
+                            </h3>
+                        <?php endif; ?>
+
+                        <div class="text-media__content-container">
+                            <?php if (!empty($text)) : ?>
+                                <div class="text-media__content-text">
+                                    <?= wp_kses_post($text) ?>
+                                </div>
+                            <?php endif; ?>
+
+                            <?php if (!empty($cta)) : ?>
+                                <a class="text-media__content-link"
+                                   href="<?= esc_url($cta['url']) ?>"
+                                   title="<?= esc_attr($cta['title']) ?>">
+                                    <?= esc_html($cta['title']) ?>
+                                </a>
+                            <?php endif; ?>
+                        </div>
+
+                        <?php if (!empty($image)) : ?>
+                            <div class="text-media__position text-media__position--<?= esc_attr($media_position) ?>">
+                                <?php
+                                if ($media_position === 'left') {
+                                    if ($shape_left === '1') {
+                                        echo '<svg class="lightblue" width="278" height="365" viewBox="0 0 278 365"><use xlink:href="#lightblue"></use></svg>';
+                                    } elseif ($shape_left === '2') {
+                                        echo '<svg class="yellow" width="298" height="203" viewBox="0 0 298 203"><use xlink:href="#yellow"></use></svg>';
+                                    }
+                                } elseif ($media_position === 'right') {
+                                    if ($shape_right === '1') {
+                                        echo '<svg class="darkblue" width="178" height="178" viewBox="0 0 178 178"><use xlink:href="#darkblue"></use></svg>';
+                                    } elseif ($shape_right === '2') {
+                                        echo '<svg class="green" width="163" height="229" viewBox="0 0 163 229"><use xlink:href="#green"></use></svg>';
+                                    }
+                                }
+                                ?>
+
+                                <?= responsive_image($image, ['classes' => 'text-media__image', 'lazy' => 'lazy']) ?>
                             </div>
                         <?php endif; ?>
+                    </section>
+                </div>
+                <?php
+            }
 
-                        <?php if (!empty($cta)) : ?>
-                            <a class="text-media__content-link"
-                               href="<?= esc_url($cta['url']) ?>"
-                               title="<?= esc_attr($cta['title']) ?>">
-                                <?= esc_html($cta['title']) ?>
-                            </a>
-                        <?php endif; ?>
-                    </div>
-
-                    <?php if (!empty($image)) : ?>
-                        <div class="text-media__position text-media__position--<?= esc_attr($media_position) ?>">
-                            <?php
-                            if ($media_position === 'left') {
-                                if ($shape_left === '1') {
-                                    echo '<svg class="lightblue" width="278" height="365" viewBox="0 0 278 365"><use xlink:href="#lightblue"></use></svg>';
-                                } elseif ($shape_left === '2') {
-                                    echo '<svg class="yellow" width="298" height="203" viewBox="0 0 298 203"><use xlink:href="#yellow"></use></svg>';
-                                }
-                            } elseif ($media_position === 'right') {
-                                if ($shape_right === '1') {
-                                    echo '<svg class="darkblue" width="178" height="178" viewBox="0 0 178 178"><use xlink:href="#darkblue"></use></svg>';
-                                } elseif ($shape_right === '2') {
-                                    echo '<svg class="green" width="163" height="229" viewBox="0 0 163 229"><use xlink:href="#green"></use></svg>';
-                                }
-                            }
-                            ?>
-
-                            <?= responsive_image($image, ['classes' => 'text-media__image', 'lazy' => 'lazy']) ?>
-                        </div>
-                    <?php endif; ?>
-                </section>
-            </div>
-            <?php
         }
         ?>
 
